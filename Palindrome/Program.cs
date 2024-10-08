4937b1eaef7c8c8725dfe1e71aeebbce640e58ca
Gaby hedges <79882381+gabyhedges@users.noreply.github.com>
2024-10-08 14:53:35 +1100

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Palindrome//names the program
+{
+    internal class Program//allows for classes
+    {
+        static void Main(string[] args)//main program. entrypoint of program
+        {
+            string inputStr;//initialises variable
+            string reverseStr = string.Empty;//initialises variable as empty
+            Console.WriteLine("Enter a string to check if it is a palindrome");//prompt to user
+            inputStr = Console.ReadLine();//accepts input and stores in variable
+            if (inputStr != null)//if statement. if input is not nothing
+            {
+                for (int i = inputStr.Length -1; i >= 0; i--)//for statement initialises integer length, condition and decrement
+                {
+                    reverseStr += inputStr[i].ToString();//reverses the string
+                }
+                if (reverseStr == inputStr)//if statement if reversed input and input match
+                {
+                    Console.WriteLine("The string is a palindrome");//if true this statement is printed
+                }
+                else
+                {
+                    Console.WriteLine("String is NOT a palindrome");//if false this statement is printed
+                }
+                    Console.ReadKey();//prevents console from closing until a key is hit
+            }
+            
+        }
+    }
+}
